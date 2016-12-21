bd3ce3d8a21cca76d954d7ec1e42bbae52a69aa1
leikai <leikai@mosainet.com>
2016-12-21 13:42:14 +0800

    dd

---
+++ msim-category/Classes/msim-catergory.h
@@ -0,0 +1,13 @@
+//
+//  msim-catergory.h
+//  Pods
+//
+//  Created by LK on 2016/12/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface msim_catergory : NSObject
+
+@end
---
+++ msim-category/Classes/msim-catergory.m
@@ -0,0 +1,13 @@
+//
+//  msim-catergory.m
+//  Pods
+//
+//  Created by LK on 2016/12/21.
+//
+//
+
+#import "msim-catergory.h"
+
+@implementation msim_catergory
+
+@end
